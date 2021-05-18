84726a3dbfbfa142c85f901ff94c5bff00e2918a
franciscofunes <ffunes90@gmail.com>
2021-05-18 18:20:59 -0300

    heroku workflow

--- sendemail.php
+++ sendemail.php
@@ -5,11 +5,12 @@
         
         public static function SendMail($to, $subject,$content){
 
-            $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-            $dotenv->load();
+            //$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+            //$dotenv->load();
             //$key = getenv('SENDGRIP_API_KEY');
-            $key = $_ENV['SENDGRID_API_KEY'];
-            
+            //$key = $_ENV['SENDGRID_API_KEY'];
+            //because i can make it work in heroku i use this api key for at least make the app work with out the email confirmation working
+            $key = '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';
 
             $email = new \SendGrid\Mail\Mail();
             $email->setFrom("info@grupodehl.com", "Grupo Dehl");
